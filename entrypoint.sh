7aa11ec5f10f7c0f89d849f96c9e07bb9f2efb41
fontaineha1281 <haduyanhwin@gmail.com>
2023-03-15 00:26:02 +0700

    Edit dockerfile and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 echo "Docker container has been started"
+composer install --ignore-platform-reqs
 chown -R www-data:www-data /var/www/web
+touch /var/log/cron.log
+chown www-data:www-data /var/log/cron.log
 nginx
+php-fpm -F
+
+# Setup a cron schedule
+echo "SHELL=/bin/bash
+* * * * * su www-data -c \"/run_schedule.sh\" -s /bin/bash www-data
+# This extra line makes it a valid cron" > scheduler.txt
+
+crontab scheduler.txt
+cron 
+tail -f /var/www/web/storage/logs/laravel*
