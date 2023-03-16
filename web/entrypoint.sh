b8ce7b9ce4e606ab5f7a6c9c360ef5dcf12eebea
fontaineha1281 <haduyanhwin@gmail.com>
2023-03-16 15:13:51 +0700

    Edit Dockerfile and entrypoint.sh

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Docker Web Server container has been started"
-chown -R www-data:www-data /var/www/web
+chown -R www-data:www-data /var/www/html
 nginx
 service php8.0-fpm start
 
