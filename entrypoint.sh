d2d2c571a778414b9fc5b6c13de6437b94812c09
fontaineha1281 <haduyanhwin@gmail.com>
2023-03-16 14:39:26 +0700

    Edit Path, Update Dockerfile, entrypoint

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Docker container has been started"
-chown -R www-data:www-data /var/www/web
-touch /var/log/cron.log
-chown www-data:www-data /var/log/cron.log
-nginx
-php-fpm -F
-
