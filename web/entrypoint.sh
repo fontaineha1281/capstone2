eb2affcc01d149f6749cadd1a290895e8f2185ac
fontaineha1281 <88372262+fontaineha1281@users.noreply.github.com>
2023-04-12 10:50:48 +0700

    Update entrypoint.sh

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Docker Web Server container has been started"
-chown -R www-data:www-data /var/www/html
+chown -R www-data:www-data /var/www/web
 nginx
-service php8.0-fpm start
-
+php-fpm -F
