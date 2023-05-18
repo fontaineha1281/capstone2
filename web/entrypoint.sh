ab41c8ea430e1ede39904d8bc63f74cd1d4adec5
fontaineha1281 <88372262+fontaineha1281@users.noreply.github.com>
2023-05-18 11:16:52 +0700

    Update entrypoint.sh

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -4,25 +4,5 @@ chown -R www-data:www-data /var/www/html
 nginx
 service php8.0-fpm start
 
-# Chạy Splunk
-${SPLUNK_HOME}/bin/splunk start --accept-license --no-prompt
-
-# Chờ Splunk khởi động hoàn toàn
-while ! ${SPLUNK_HOME}/bin/splunk status | grep -q "splunkd is running"; do
-  sleep 2
-done
-
-# Cấu hình Splunk để chạy với quyền root (khuyến khích trong container)
-${SPLUNK_HOME}/bin/splunk edit user admin -password haduyanh -role admin -auth admin:changeme
-
-# Dừng Splunk
-${SPLUNK_HOME}/bin/splunk stop
-
-# Chạy Splunk lại với quyền root
-${SPLUNK_HOME}/bin/splunk start --accept-license --no-prompt --answer-yes --seed-passwd haduyanh
-
-# Để Splunk chạy ngầm trong container
-${SPLUNK_HOME}/bin/splunk enable boot-start -user root
-
 # Giữ container chạy
 tail -f /dev/null
